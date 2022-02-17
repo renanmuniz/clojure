c26baddbd2642229b5517996b908f11857214082
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-17 17:55:47 -0300

    Funcoes anonimas

--- clojure-estudo/src/clojure_estudo/functions.clj
+++ clojure-estudo/src/clojure_estudo/functions.clj
@@ -1,6 +1,6 @@
 (ns clojure-estudo.functions)
 
-; Functions
+; Funcoes
 
 (defn Funcao                                                ;declaracao da funcao
   "Exibe os valores informados"                             ;'descritivo' (javadoc)
@@ -25,4 +25,35 @@
   )
 
 (FuncaoCalculoCombinacoes 10)
+
+
+; Funcoes anonimas
+(defn ExecutaUmaFuncaoAnonima []
+  ((fn [x] (* 2 x)) 2))                                     ;funcao anonima
+(ExecutaUmaFuncaoAnonima)
+
+
+(defn ExecutaUmaFuncaoAnonima2 []
+  (def x ( (fn [x] (* 2 x)) 2))                                     ;funcao anonima
+  (println x)
+)
+(ExecutaUmaFuncaoAnonima2)
+
+
+(defn ExecutaUmaFuncaoAnonima3
+  [v1 v2 v3]
+  (println ((fn [a b c] (+ a b c)) v1 v2 v3))
+  )
+(ExecutaUmaFuncaoAnonima3 2 7 5)
+
+
+
+(defn ExecutaUmaFuncaoAnonima4
+  "Formula de Bhaskara"
+  [v1 v2 v3]
+  (println ((fn [a b c] (- (* b b) (* 4 a c))) v1 v2 v3))
+  )
+(ExecutaUmaFuncaoAnonima4 4 2 -6)
+
+
 
