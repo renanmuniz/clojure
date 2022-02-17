cd3a86f2db94fc4c3fe4a4a00c5ca66a1b36e2b3
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-17 15:08:21 -0300

    Condicionais

--- clojure-estudo/src/clojure_estudo/condicionais.clj
+++ clojure-estudo/src/clojure_estudo/condicionais.clj
@@ -69,7 +69,72 @@
 (TesteIfAninhado2)
 
 
+
 ; Case
 
+(defn TesteCase []
+  (def x 5)
+  (case x 5 (println "x is 5")
+          10 (println "x is 10")
+          (println "x is neither 5 nor 10")))
+(TesteCase)
+
+
+(defn TesteCase2 []
+  (def var 6)
+  (case var 1 (println " = 1")
+            2 (println " = 2")
+            3 (println " = 3")
+            4 (println " = 4")
+            5 (println " = 5")
+            (println "Nenhum desses")
+            )
+)
+(TesteCase2)
+
+(defn TesteCase3 []
+  (def var 6)
+  (case var 1 (println " = 1")
+            2 (println " = 2")
+            3 (println " = 3")
+            4 (println " = 4")
+            5 (println " = 5")
+            (println "Nenhum desses")
+            )
+  )
+(TesteCase3)
+
+
+; cond     ;executa verificacoes e executa assim que encontrar alguma que satisfaca
+; as outras condicoes nao sao avaliadas apos alguma anterior satisfazer a condicao.
+
+(defn TesteCond []
+  (def x 10)
+  (cond
+    (= 0 (rem x 2)) (println "é par")
+    (= x 5) (println "x is 5")
+    (= x 10)(println "x is 10")
+    :else (println "x is not defined")))
+(TesteCond)
+
+
+(defn TesteCond2 []
+  (def x 10)
+  (cond
+    (= x 5) (println "x is 5")
+    (= x 10)(println "x is 10")
+    (= 0 (rem x 2)) (println "é par")
+    :else (println "x is not defined")))
+(TesteCond2)
+
+
+(defn TesteCond3 []
+  (def x 11)
+  (cond
+    (= x 5) (println "x is 5")
+    (= x 10)(println "x is 10")
+    (= 0 (rem x 2)) (println "é par")
+    :else (println "x is not defined")))
+(TesteCond3)
 
 
