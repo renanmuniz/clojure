3fc870b1ffd54c37bd1c8ca15884416f43b82848
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-17 16:35:53 -0300

    Functions

--- clojure-estudo/src/clojure_estudo/functions.clj
+++ clojure-estudo/src/clojure_estudo/functions.clj
@@ -1,3 +1,28 @@
 (ns clojure-estudo.functions)
 
+; Functions
+
+(defn Funcao                                                ;declaracao da funcao
+  "Exibe os valores informados"                             ;'descritivo' (javadoc)
+  [x y]                                                     ;parametros esperados
+  (println "valores: " x y))                                ;bloco de codigo
+
+(Funcao 2 3)                                                ;chamada da funcao passando os parametros
+
+
+(defn FuncaoSomaEdividePorDois
+  "Soma dois numeros e divide o resultado por dois"
+  [x y]
+  (/ (+ x y) 2))
+
+(FuncaoSomaEdividePorDois 3 5)
+
+
+(defn FuncaoCalculoCombinacoes
+  "Com base na entrada do numero de opcoes calcula no numero de combinacoes possiveis"
+  [x]
+  (* x (- x 1))
+  )
+
+(FuncaoCalculoCombinacoes 10)
 
