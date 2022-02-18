fdd95c2ea26402d5abaf7cbaf7b7f573b2b546a0
Renan Merlo <renan.merlo@paygo.com.br>
2022-02-18 14:39:51 -0300

    High-order Functions

--- clojure-estudo/src/clojure_estudo/functions.clj
+++ clojure-estudo/src/clojure_estudo/functions.clj
@@ -56,4 +56,53 @@
 (ExecutaUmaFuncaoAnonima4 4 2 -6)
 
 
+; Funcao com multiplos parametros
 
+(defn FuncaoMultiplosParametros
+  "Funcao que recebe 3 parametros e soma-os"
+  [a b c]
+  (+ a b c)
+  )
+(FuncaoMultiplosParametros 3 4 5)
+
+
+; Variadic Functions
+
+(defn FuncaoArgumentosVariaveis
+  [nome & mensagem]
+  (println "Olá " nome "," mensagem)
+)
+(FuncaoArgumentosVariaveis "Renan" "bem-vindo ao Clojure!" "tutorialspoint.com" "funcao com argumentos" "variaveis")
+
+
+; High-Order Functions
+; sao funcoes que recebem outras funcoes como argumento
+
+(println (+ 1 (* 2 3)))
+
+(defn FuncaoDeAltaOrdem
+  "Funcao que usa como argumento outra funcao"
+  [x y]
+  (+ 10 (* x y))
+  )
+(FuncaoDeAltaOrdem 2 3)
+
+(defn FuncaoDeAltaOrdem2
+  "Funcao que usa como argumento outra funcao"
+  [x]
+  (defn soma1 [x] (+ 1 x))
+  (soma1 x)
+  )
+(FuncaoDeAltaOrdem2 2)
+
+(defn FuncaoDeAltaOrdem3
+  [a b]
+  (+ a b)
+  )
+(FuncaoDeAltaOrdem3 (+ 2 2) (+ 3 4))
+
+(defn FuncaoDeAltaOrdem4
+  [a b]
+  (* (+ a b) (* a b))
+  )
+(FuncaoDeAltaOrdem4 (+ 2 2) (* 2 (+ 3 4)))
